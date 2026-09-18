30e425105ecfb2c92f6b6424400609a2fad3944f
Husnain Ali <husnain_ali@mpn-soft.com>
2026-09-18 11:30:16 -0700

    Create ErrorViewModel

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BlogEngine.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
