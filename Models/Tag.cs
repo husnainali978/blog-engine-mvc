95efe00529b73f99b683ebecbece79a32af4d370
Husnain Ali <husnain_ali@mpn-soft.com>
2026-09-18 11:30:18 -0700

    Create Tag

---
+++ Models/Tag.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogEngine.Mvc.Models;
+
+/// <summary>
+/// A category/tag that posts can be grouped and browsed by.
+/// </summary>
+public class Tag
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, StringLength(50)]
+    public string Slug { get; set; } = string.Empty;
+
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+}
